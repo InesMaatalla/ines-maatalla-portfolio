b93e40eb5e7d23af79c377e7f24449968c732881
inesmaatalla <Ines.Maatalla@hotmail.fr>
2020-11-12 03:06:46 -0500

    modified json

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import Stacks from "../components/Stacks"
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/HeroSection"
-import Projects from "../components/Project"
+import Projects from "../components/Projects"
 import Footer from "../components/Footer"
 import Layout from "../components/Layout"
 
