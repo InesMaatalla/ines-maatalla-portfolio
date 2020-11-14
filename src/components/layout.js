407ea69320a12bb660b78e9303aed030e108566b
inesmaatalla <Ines.Maatalla@hotmail.fr>
2020-11-14 05:06:51 -0500

    nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./layout.css"
 import Navbar from "./NavBar"
-
+// import Home from './components/pages/HomePage/Home';
+// import Projects from './components/pages/Projects/Projects';
+// import Stacks from './components/pages/Stacks/Stacks';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Navbar/>
       <main>{children}</main>
+     
     </div>
   )   
 }
