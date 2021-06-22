8ecb0545e22dd1089d9902f323e10a34980999ee
inesmaatalla <Ines.Maatalla@hotmail.fr>
2021-06-22 23:33:14 +0200

    read me added

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -6,7 +6,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/inesmaatalla/ines-maatalla-portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"Ines Maatalla - Portfolio","short_name":"Ines Maatalla","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/inesphoto.jpg","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":null},
+      options: {"plugins":[],"name":"Ines Maatalla - Portfolio","short_name":"Ines Maatalla","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/inesphoto.jpeg","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"fcad040bc26d6a9e7dd99bd1b7dd3a72"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
